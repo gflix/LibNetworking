dfb5ead8e6a1c40d9e2f5701db7074f0f746cfd9
Felix Mellmann <felix.mellmann@gmail.com>
2018-02-16 21:28:21 +0100

    Finished tests for the SHA1 implementation. Incremented to version 0.1.2

--- tests/TestSha1.cpp
+++ tests/TestSha1.cpp
@@ -1,11 +1,31 @@
 #include <gtest/gtest.h>
+#include <networking/Conversion.h>
 #include <networking/digest/Sha1.h>
 
 TEST(Sha1, EmptyMessageHash)
 {
     Flix::Sha1 sha1;
 
+    std::string expected { "da39a3ee5e6b4b0d3255bfef95601890afd80709" };
+
     sha1.reset();
-    EXPECT_EQ(sha1.getHash(),
-        "\xda\x39\xa3\xee\x5e\x6b\x4b\x0d\x32\x55\xbf\xef\x95\x60\x18\x90\xaf\xd8\x07\x09");
+    EXPECT_EQ(Flix::Conversion::binToHex(sha1.getHash()), expected);
+}
+
+TEST(Sha1, TestMessageHash)
+{
+    Flix::Sha1 sha1;
+
+    std::string messageA { "test" };
+    std::string expectedA { "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3" };
+    std::string messageB { "1234" };
+    std::string expectedB { "9bc34549d565d9505b287de0cd20ac77be1d3f2c" };
+    std::string expectedC { "test1234" };
+
+    sha1.reset();
+    sha1.appendMessage(messageA);
+    EXPECT_EQ(Flix::Conversion::binToHex(sha1.getHash()), expectedA);
+    sha1.appendMessage(messageB);
+    EXPECT_EQ(Flix::Conversion::binToHex(sha1.getHash()), expectedB);
+    EXPECT_EQ(sha1.getMessage(), expectedC);
 }
